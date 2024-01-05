fc16f99af72fb1a44f3a621b0e2bf247b259126b
Anton Ivanov <anton.ivanov@cambridgegreys.com>
2024-01-05 12:38:59 +0000

    Output utility usable for iptables input
    Signed-off-by: Anton Ivanov <anton.ivanov@cambridgegreys.com>

---
+++ compile.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+
+'''Generic compiler frontend'''
+
+import sys
+from argparse import ArgumentParser
+import json
+import pcap_parser
+from code_objects import finalize, ProgramEncoder
+import bpf_objects
+
+HELPERS = {
+    "cbpf":bpf_objects.dispatcher,
+}
+
+
+def main():
+    '''Parse a pcap expression and compile it into
+    bpf
+    '''
+    aparser = ArgumentParser(description=main.__doc__)
+    aparser.add_argument(
+       '--expression',
+        help='pcap expression',
+        type=str
+        )
+    aparser.add_argument(
+       '--debug',
+        help='debug',
+        type=bool
+        )
+    aparser.add_argument(
+       '--output',
+        help='output file, if absent - stdout',
+        type=str
+        )
+    aparser.add_argument(
+       '--generators',
+        help='code generators cbpf, etc',
+        action="append",
+        type=str
+        )
+    aparser.add_argument(
+       '--format',
+        help='output format asm, iptables, binary',
+        type=str,
+        default="asm"
+        )
+    args = vars(aparser.parse_args())
+
+    try:
+        debug = args["debug"]
+    except KeyError:
+        debug = False
+
+    try:
+        generators = args["generators"]
+    except KeyError:
+        generators = ["cbpf"]
+
+    if generators is None:
+        generators = ["cbpf"]
+
+    parsed = finalize(pcap_parser.PARSER.parse(args["expression"]))
+
+    if debug:
+        sys.stderr.write(json.dumps(parsed, cls=ProgramEncoder, indent=4))
+        sys.stderr.write("\n")
+
+    for generator in generators:
+        parsed.add_helper(HELPERS[generator])
+
+    parsed.compile(bpf_objects.CBPFCompilerState()) # FIXME - make generic
+
+    if debug:
+        for generator in generators:
+            counter = 0
+            for inst in parsed.get_code(generator):
+                sys.stderr.write(f"{counter} {inst}\n")
+                counter += 1
+
+    parsed.resolve_refs()
+
+    try:
+        out = open(args["output"],"+w",encoding="ascii")
+    except (KeyError, TypeError):
+        out = sys.stdout
+
+    if args["format"] == "asm":
+        for generator in generators:
+            counter = 0
+            for inst in parsed.get_code(generator):
+                out.write(f"{counter} {inst}\n")
+                counter += 1
+    elif args["format"] == "iptables":
+        for generator in generators:
+            try:
+                code = parsed.get_code(generator)
+                out.write("{}".format(len(code)))
+                counter = 0
+                for insn in code:
+                    out.write(", {} {} {} {}".format(*insn.obj_dump(counter)))
+                    counter += 1
+                out.write("\n")
+            except KeyError:
+                pass
+    else:
+        sys.stderr.write("This output is not yet supported\n")
+
+    if out != sys.stdout:
+        out.close()
+
+
+if __name__ == "__main__":
+    main()
